75375d567466672296e92b9318168586eb3b4486
Dino <dino.budic@gmail.com>
2024-09-12 20:40:57 +0200

    Android platform.. Testing if sqlite works

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 
 ## Plugins used
 - @capacitor-community/sqlite
+
+
+### Commands you need often
+- ionic cap add [platform]
+- ionic cap sync [platform] or without specified platform
+- ionic cap open android (just opens android app in android studio)
+- ionic cap build [platform] (android in my case)
+- ionic cap run android --livereload --external --public-host=192.168.1.52
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection } from '@capacitor-community/sqlite';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.testDatabase();
+  }
+
+  async testDatabase() {
+    const sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite);
+
+    let db: SQLiteDBConnection = await sqlite.createConnection('appdb', false, 'no-encryption', 1, false);
+
+    try {
+      await db.open();
+      // after successful db open we can work with our DB
+
+      const schema = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email TEXT UNIQUE NOT NULL, age INTEGER);`;
+
+      let result = await db.execute(schema); // execute sql
+
+      const users = `
+      DELETE FROM users;
+      INSERT INTO users (id, email, age) VALUES (1, 'aa@bb', 31);
+      INSERT INTO users (id, email, age) VALUES (2, 'bb@cc', 34);
+      INSERT INTO users (id, email, age) VALUES (3, 'cc@dd', 29);
+      `;
+
+      result = await db.execute(users);
+
+      console.log("Users insert Result ", result);
+      
+      const res = await db.query('SELECT * FROM users');
+
+      console.log('Users from DB ', JSON.stringify(res));
+
+      await sqlite.closeAllConnections();
+      
+
+    } catch (e) {
+      console.error('ERROR: ', e);
+      await sqlite.closeAllConnections();
+      // close connections to avoid problems if DB already has connection hanging
+    }
+  }
 }
